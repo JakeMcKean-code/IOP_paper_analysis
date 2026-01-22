8140554ab353271a80623cdedf159bae32bf5d6a
JakeMcKean-code <jake.mckean12@gmail.com>
2026-01-22 15:08:01 +0900

    Add plot to show Enu_QE just for a given channel to compare against literature

--- scripts/Fig1_SF_HK_EnuRecoOnly.py
+++ scripts/Fig1_SF_HK_EnuRecoOnly.py
@@ -3,17 +3,16 @@ from FlatTreeMod import *
 custom_lines = []
 labels = []
 
-def plot_EnuReco_bias(ax, filename: str, label: str, color: str):
+def plot_EnuReco_osc(ax, filename: str, label: str, color: str):
 
     infile = up.open(filename)
     Enu_t  = 1000*infile["FlatTree_VARS;1"]["Enu_true"].array()
     Enu_QE = 1000*infile["FlatTree_VARS;1"]["Enu_QE"].array()
-    mode   = infile["FlatTree_VARS;1"]["cc"].array()
-    diff = Enu_t - Enu_QE
+    # mode   = infile["FlatTree_VARS;1"]["cc"].array()
+    mode   = infile["FlatTree_VARS;1"]["Mode"].array()
 
     # Select only events with mode == 1
     mask = (mode == 1)
-    diff_sel = diff[mask]
     Enu_t_sel = Enu_t[mask]
     Enu_QE_sel = Enu_QE[mask]
 
@@ -25,13 +24,13 @@ def plot_EnuReco_bias(ax, filename: str, label: str, color: str):
 
 
 
-    ax.hist(diff_sel, bins=np.arange(-1000, 1000, step=10), histtype='step', weights=np.ones_like(diff_sel), color=color,linewidth=1.5, label = label)
+    ax.hist(Enu_QE_sel, bins=np.arange(0, 2000, step=10), histtype='step', weights=np.ones_like(Enu_QE_sel), color=color,linewidth=1.5, label = label)
     # Create a matching line handle for legend
     custom_lines.append(Line2D([0], [0], color=color, lw=2, linestyle='-'))
     labels.append(label)
 
     color = vivid_purple
-    ax.hist(diff_sel, bins=np.arange(-1000, 1000, step=10), histtype='step', weights=weights, color=color,linewidth=1.5, label = label, linestyle = '--')
+    ax.hist(Enu_QE_sel, bins=np.arange(0, 2000, step=10), histtype='step', weights=weights, color=color,linewidth=1.5, label = label, linestyle = '--')
     # Create a matching line handle for legend
     custom_lines.append(Line2D([0], [0], color=color, lw=2, linestyle='--'))
     labels.append(label+" osc")
@@ -41,11 +40,11 @@ def plot_EnuReco_bias(ax, filename: str, label: str, color: str):
 
 
 fig, ax = plt.subplots()
-plot_EnuReco_bias(ax, "../flattrees/nuwro/NuWro_HK_test.flat.root", r"NuWro SF", dark_blue)
+plot_EnuReco_osc(ax, "../flattrees/nuwro/NuWro_HK_test.flat.root", r"NuWro SF", dark_blue)
 
 plt.legend(custom_lines, labels)
-plt.title("HK Enu reco for all channels")
-plt.xlabel(r"$E_{\nu}^{\text{true}} - E_{\nu}^{\text{QE}}$ [MeV]")
+plt.title("HK oscillated Enu reco for all channels")
+plt.xlabel(r"$E_{\nu}^{\text{true}}$ [MeV]")
 plt.ylabel("Number of events")
 plt.show()
 
